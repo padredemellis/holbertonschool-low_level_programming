6c1112b57917bc78a7a86581e469df1ee35cc046
padredemellis <10656@holbertonstudents.com>
2025-02-14 19:41:22 +0000

    C - Hello, World4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+/*La funcion Main sirve para ejecutar la parte principal de un programa*/
 int main(void)
 {
 	char frase[] = "Programming is like building a multilingual puzzle";
