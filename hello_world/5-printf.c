d8f0d012c633c95a7116b79d87f25c610ebe9a32
padredemellis <10656@holbertonstudents.com>
2025-02-14 20:56:14 +0000

    C - Hello, World5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/*Esto es un comentario de lo que va a suceder en int Main()*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
