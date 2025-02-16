26019e91feb8a523d7e226bffdb3077b3d360c50
padredemellis <10656@holbertonstudents.com>
2025-02-16 22:01:19 +0000

    C - Hello, World4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/* Esto es un comentario*/
+/**
+ * Esto es un comentario
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-       	return (0);
+	return (0);
 }
