16f04be2d988237ca3a73a2b610f318322bfc117
padredemellis <10656@holbertonstudents.com>
2025-02-17 16:39:32 +0000

    C - Variables, if, else, while6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Print digit numbers
+* Description: Prints all single digit numbers of base 10 starting from 0
+* Return: return 0
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar (n + '0');
+		n++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
