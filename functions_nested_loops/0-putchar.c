e62878140274755c0c48ff8bfe759dcde45b9b83
padredemellis <10656@holbertonstudents.com>
2025-02-18 13:15:45 +0000

    C - Functions, nested loops  Novice0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<main.h>
 /**
 * main - Print an word
 * Description: Print _putchar
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prototipo de _putchar */
+int _putchar(char c);
+
+/* Prototipos de tus funciones */
+int mi_funcion_1(int a, int b);
+void mi_funcion_2(char *str);
+int mi_funcion_3(void);
+
+#endif /* MAIN_H */
