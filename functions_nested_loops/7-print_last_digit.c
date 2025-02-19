ed3630f09052c9c82bfb4f4317dfed147864c569
padredemellis <10656@holbertonstudents.com>
2025-02-19 16:07:24 +0000

    C - Functions, nested loops7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - Mostrar el ultimo numero.
+ * @n: El número a chequear.
+ *
+ * Return: abs(numero)%10.
+ */
+int print_last_digit(int n)
+{
+	return (abs(n) % 10);
+}
