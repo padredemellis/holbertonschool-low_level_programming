494774980a0145c47725f3ee62b21490f452b1fe
padredemellis <10656@holbertonstudents.com>
2025-02-21 00:19:19 +0000

    C - Functions, nested loops10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * add - sumar dos numeros.
+ * void: hacer cuentas.
+ * @a: num1
+ * @b: num2
+ * Return: resultado.
+ *
+ */
+int add(int a, int b)
+{
+		return (a + b);
+}
