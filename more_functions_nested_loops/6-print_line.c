18c54a42883f5818b253e42fdb9121af92ec9273
padredemellis <10656@holbertonstudents.com>
2025-02-24 16:00:01 +0000

    C - More functions, more nested loops6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_line - Multiplies two integers.
+ * @n: is a integer.
+ *
+ * Return: a line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for(i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
