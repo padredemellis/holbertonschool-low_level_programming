be3b5125efa053e38fa1ccac7ed46474d9024b1b
padredemellis <10656@holbertonstudents.com>
2025-02-25 10:41:34 +0000

    C - More functions, more nested loops  Novice10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_triangle - print a triangle.
+ * @size: is a size.
+ *
+ * Return: a line
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
