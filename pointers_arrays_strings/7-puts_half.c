cb4311d547697dba2dfe3afa0cc0adfb2fe8ac46
padredemellis <10656@holbertonstudents.com>
2025-02-27 15:07:32 +0000

    C - Pointers, arrays and strings7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+void puts_half(char *str)
+{
+	int len = 0, n = 0, c =0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	while (n >= len / 2)
+	{
+		_putchar(n);
+		n++
+	{
+	if (s % 2 == 1)
+		_putchar(c = len + 1/2);
+	}
+}
