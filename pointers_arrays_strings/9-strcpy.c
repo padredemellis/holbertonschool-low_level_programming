839545e4e52b7bb8d1187b13c81f639e0d5ea28e
padredemellis <10656@holbertonstudents.com>
2025-02-27 17:14:12 +0000

    C - Pointers, arrays and strings9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,21 @@
-char *_strcpy(char *dest, char *src);
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - Copia una cadena de texto de `src` a `dest`.
+ * @dest: Donde vamos a copiar la cadena.
+ * @src: La cadena que queremos copiar.
+ * Return: Un puntero a `dest`.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
