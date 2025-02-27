bb25f13eedb55beb3f27f04016891007f2126287
padredemellis <10656@holbertonstudents.com>
2025-02-27 15:36:40 +0000

    C - Pointers, arrays and strings7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,16 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - Make a function that prints half of a string.
+ * @str: Is a ponter.
+ */
 void puts_half(char *str)
 {
-	int len = 0, n = 0, c =0;
+	int len = 0, n = 0, i = 0;
 
-	while (s[len] != '\0')
+	while (str[len] != '\0')
 	{
 		len++;
-	while (n >= len / 2)
-	{
-		_putchar(n);
-		n++
-	{
-	if (s % 2 == 1)
-		_putchar(c = len + 1/2);
 	}
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len + 1 / 2);
+	}
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
