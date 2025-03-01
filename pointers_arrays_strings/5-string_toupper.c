0982508933a53b86d0f449bea75161ff83e2da39
padredemellis <10656@holbertonstudents.com>
2025-03-01 18:30:40 +0000

    More pointers, arrays and strings

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - Convierte todas las letras minúsculas a mayúsculas.
+ * @str: La cadena a modificar.
+ *
+ * Return: Puntero a la cadena modificada.
+ */
+char *string_toupper(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
