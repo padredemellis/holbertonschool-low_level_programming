77c099c71d790f5726b2c4195c6a819039651aa4
padredemellis <10656@holbertonstudents.com>
2025-03-05 18:26:19 +0000

    argc, argv0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ * @argc: número de argumentos que se pasaron al programa
+ * @argv:un array de punteros a caracteres (strings).
+ * Return: Always 0.
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+
+	return (0);
+}
