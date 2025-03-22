cfe938942ded1c9e690599471552f85b042e93d5
padredemellis <10656@holbertonstudents.com>
2025-03-22 07:34:09 +0000

    C - Variadic functions0

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - Imprime c,i,f,s
+ * @format:la cadena a imprimir
+ * @...: Parámetros variables.
+ */
+void print_all(const char  * const format, ...)
+{
+
