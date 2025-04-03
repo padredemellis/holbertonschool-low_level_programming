b25ab01382fba95ba3299cc8befe066abfa1c7b2
padredemellis <10656@holbertonstudents.com>
2025-04-03 10:40:29 +0000

    C - Singly linked lists2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,27 +1,42 @@
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
-* add_node - Añade un nuevo nodo al principio de una lista list_t.
-* @head: Puntero doble al principio de la lista.
-* @str: Cadena que se duplicará y añadirá al nuevo nodo.
-*
-* Return: Dirección del nuevo elemento o NULL si falló.
-*/
+ * _strlen - Calcula la longitud de una cadena
+ * @s: Cadena a medir
+ * Return: Longitud de la cadena
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * add_node - Añade un nuevo nodo al principio de la lista
+ * @head: Puntero doble al inicio de la lista
+ * @str: Cadena para el nuevo nodo
+ * Return: Dirección del nuevo nodo o NULL si falla
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *nuevo;
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
+
 	nuevo = malloc(sizeof(list_t));
 	if (nuevo == NULL)
-	{
 		return (NULL);
-	}
+
 	nuevo->str = strdup(str);
 	if (nuevo->str == NULL)
 	{
