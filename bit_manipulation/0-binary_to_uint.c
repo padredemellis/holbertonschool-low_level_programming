80137721f900d97aad9e61ffaf049d6055ff6965
padredemellis <10656@holbertonstudents.com>
2025-04-05 23:15:09 +0000

    C - Bit manipulation0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * binary_to_uint - Convierte una cadena binaria a un número entero sin signo.
+ * @b: Puntero a una cadena de caracteres que contiene '0' y '1'.
+ *
+ * Return: El número convertido o 0 si hay un carácter inválido o b es NULL.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0
+	int i;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		result = result * 2 + (b[i] - '0');
+	}
+
+	return (result);
+}
+
